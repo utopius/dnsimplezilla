7f69561cd7fa894a6b8d8a938f885a5e47f0d229
Florian Herbel <2223097+utopius@users.noreply.github.com>
2014-06-07 21:48:52 +0200

    ZIL-16 broke the project file (great...)

--- DNSimple.UpdateService/EventId.cs
+++ DNSimple.UpdateService/EventId.cs
@@ -2,6 +2,6 @@
 {
     public enum EventId
     {
-        Info = 0, RecordUpdated = 10, RecordNotUpdated = 20, Warning = 1000, Error = 2000
+        Info = 0, RecordUpdated = 10, RecordUpdateNotNecessary = 20, Warning = 1000, Error = 2000
     }
 }
