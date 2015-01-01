299f07d095a29141c50868b50a48d60f6c093e66
Holger Steffan <9589371+hsteffan@users.noreply.github.com>
2015-01-01 02:53:24 +0100

    Adjusted build directories Now using WiX.Toolset NuGet package to build the installer

--- build.fsx
+++ build.fsx
@@ -6,41 +6,34 @@ open Fake
 let currentVersion =
   if not isLocalBuild then buildVersion else
   "0.0.0.1"
-let buildDir = "./build/Release"
-let setupDir = "./setup/"
+let buildDir = "./build/"
+let appDir = buildDir + "Release/"
+let deployDir = buildDir + "deploy/"
 let buildReferences = !! "src/app/**/*.csproj"
 let setupReferences = !! "src/app/**/*.wixproj"
 let setupBinaries = !! (buildDir + "/**/*.msi")
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; setupDir]
+    CleanDirs [appDir; deployDir]
 )
 
 Target "Build" (fun _ ->
     buildReferences
-      |> MSBuildRelease buildDir "Build"
+      |> MSBuildRelease appDir "Build"
       |> Log "AppBuild-Output: "
 )
 
-Target "Setup" (fun _ ->
+Target "Deploy" (fun _ ->
     setupReferences
-      |> MSBuildReleaseExt buildDir ["Version", currentVersion] "Build"
+      |> MSBuildReleaseExt deployDir ["Version", currentVersion] "Build"
       |> Log "SetupBuild-Output: "
-    // Copy all important files to the deploy directory
-    setupBinaries
-      |> Copy setupDir 
-)
-
-Target "Default" (fun _ ->
-    trace "Hello World from FAKE"
 )
 
 // Dependencies
 "Clean"
   ==> "Build"
-  ==> "Default"
-  ==> "Setup"
+  ==> "Deploy"
 
 // start build
-RunTargetOrDefault "Default"
+RunTargetOrDefault "Build"
