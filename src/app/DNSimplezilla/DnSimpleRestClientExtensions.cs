98dacb1cebe0d5addb1e39a148cf0bc2a40c85d0
Florian Herbel <2223097+utopius@users.noreply.github.com>
2016-06-19 15:33:37 +0200

    store the last known ip and compare it on the next run icanhazip.com support and fucking ipv6 hazzle

--- src/app/DNSimplezilla/DnSimpleRestClientExtensions.cs
+++ src/app/DNSimplezilla/DnSimpleRestClientExtensions.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using DNSimple;
 
 namespace DNSimplezilla
 {
-    static class DnSimpleRestClientExtensions
+    public interface IDnSimple
     {
-        public static IEnumerable<DomainRecord> GetDnsRecords(this DNSimpleRestClient client, Domain domain)
+        Task<IEnumerable<DomainRecord>> GetDnsRecordsAsync(Domain domain);
+        Task UpdateRecordAsync(Domain domain, DomainRecord hostRecord, IPAddress publicIp);
+    }
+
+    class DnSimple : IDnSimple
+    {
+        private readonly DNSimpleRestClient _client;
+
+        public DnSimple(DNSimpleRestClient client)
         {
-            dynamic records = client.ListRecords(domain.Name);
-            var enumerable = (IEnumerable<dynamic>)records;
-            return enumerable.Select(x => x.record)
-                             .Select(record => DomainRecord.FromDynamic(record))
-                             .Cast<DomainRecord>();
+            _client = client;
+        }
+
+        public async Task<IEnumerable<DomainRecord>> GetDnsRecordsAsync(Domain domain)
+        {
+            return await Task.Run(() =>
+            {
+                dynamic records = _client.ListRecords(domain.Name);
+                var enumerable = (IEnumerable<dynamic>) records;
+                return enumerable.Select(x => x.record)
+                                 .Select(record => DomainRecord.FromDynamic(record))
+                                 .Cast<DomainRecord>();
+            });
+        }
+
+        public async Task UpdateRecordAsync(Domain domain, DomainRecord hostRecord, IPAddress publicIp)
+        {
+            await Task.Run(() => _client.UpdateRecord(domain.Name, (int) hostRecord.Id, hostRecord.Name, publicIp.ToString()));
         }
     }
 }
